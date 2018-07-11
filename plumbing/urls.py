ac914875098f31bc0596ce33a9baa5d634351c29
BartoszLewosz <bartosz.lewosz@gmail.com>
2018-07-11 14:42:16 +0100

    Fixed plumbing detail template

--- plumbing/urls.py
+++ plumbing/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.views.generic import DetailView
-from plumbing.models import Plumbing
+from .models import Plumbing
 from . import views
 
 urlpatterns = [
@@ -8,8 +8,8 @@ urlpatterns = [
 	url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Plumbing,
 		template_name='plumbing/plumbing_detail.html')),
 	url(r'^plumbing_new/', views.plumbing_new, name='plumbing_new'),
-	url(r'(?P<pk>\d+)/edit/$', views.plumbing_edit, name='plumbing_edit'),
-	url(r'(?P<pk>\d+)/delete/$', views.plumbing_delete, name='plumbing_delete'),
+	url(r'^(?P<pk>\d+)/edit/$', views.plumbing_edit, name='plumbing_edit'),
+	url(r'^(?P<pk>\d+)/delete/$', views.plumbing_delete, name='plumbing_delete'),
 	url(r'^plumbing_print/', views.plumbing_print, name='plumbing_print'),
 
 ]
