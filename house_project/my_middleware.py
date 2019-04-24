7cc99419eb58d41a718a779411d6fcfe841466c0
Bartosz Lewosz <bartosz.lewosz@gmail.com>
2019-04-24 19:03:18 +0100

    Add EXEMPT_URLs to middleware.

--- house_project/my_middleware.py
+++ house_project/my_middleware.py
@@ -3,6 +3,11 @@ import re
 from django.conf import settings
 from django.shortcuts import redirect
 
+
+EXEMPT_URLS = [re.compile(settings.LOGIN_URL.lstrip("/"))]
+if hasattr(settings, 'LOGIN_EXEMPT_URL'):
+	EXEMPT_URLS += [re.compile(url) for url in settings.LOGIN_EXEMPT_URL]
+
 class LoginRequiredMiddleware:
 	
 	def __init__(self, get_response):
@@ -15,7 +20,9 @@ class LoginRequiredMiddleware:
 	
 	def process_view(self, request, view_func, view_args, view_kwargs):
 		assert hasattr(request, 'user')
+		path = request.path_info.lstrip('/')
+		print(path)
 
 		if not request.user.is_authenticated:
-			if True:
+			if not any(url.match(path) for url in EXEMPT_URLS):
 				return redirect(settings.LOGIN_URL)
