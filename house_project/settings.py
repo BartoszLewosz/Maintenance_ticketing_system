32984e4b0e0aa38bff4cc67cbcef52999146dbc8
Bartosz Lewosz <bartosz.lewosz@gmail.com>
2019-04-25 20:53:10 +0100

    Add custom middleware.
    For Login required

--- house_project/settings.py
+++ house_project/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'house_project.my_middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'house_project.urls'
